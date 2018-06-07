0cc13c07de2e593bdfe885e57e79be43da0e4e87
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2018-06-06 22:05:55 -0700

    refactor

---
+++ get_features.m
@@ -0,0 +1,18 @@
+function [Features] = get_features(sift_dir, train_size)
+%CREATE_FEATURES Summary of this function goes here
+%   Detailed explanation goes here
+    fnames = dir([sift_dir '/*.mat']);
+    num_feats = length(fnames);
+
+    fnames_vector = randperm(num_feats, train_size); %6612 is the number of images in fnames and 500 distinct images are randomly picked
+
+    Features = [];
+    for i = 1:train_size
+        fname = [sift_dir '/' fnames(fnames_vector(i)).name];
+        load(fname, 'descriptors');
+        Features= [Features; descriptors];
+    end
+    save('features.mat', 'features');
+end
+
+s
--- visualize_vocabulary.m
+++ visualize_vocabulary.m
@@ -1,67 +1,51 @@
 addpath('./provided_code/');
-framesdir = './frames/';
-siftdir = './sift/';
+frames_dir = './frames/';
+sift_dir = './sift/';
+
+train_size = 500;
+Features = get_features(sift_dir, train_size);
  
-fnames = dir([siftdir '/*.mat']);
- 
-fnames_vector = randperm(6612, 500); %6612 is the number of images in fnames and 500 distinct images are randomly picked
- 
-feature_matrix = [];
- 
-for i = 1:500
-	fname = [siftdir '/' fnames(fnames_vector(i)).name];
-	load(fname, 'descriptors');
-	feature_matrix= [feature_matrix; descriptors];
-end
- 
-[membership,means,rms] = kmeansML(1500,transpose(feature_matrix));
-save('kMeans.mat', 'means');
+[~,means,~] = kmeansML(1500,Features');
+% save('kMeans.mat', 'means');
  
 x = randperm(size(means,2),2);
 words = means(:,x);
 words = transpose(words);
 
-Word_1 = figure;
-Word_2 = figure;
-figure(Word_1);
+f1 = figure;
+f2 = figure;
+figure(f1);
 
-fnames_vector_2 = randperm(6612,25);
+fnames_vector = randperm(num_feats,25);
 
 for i = 1:25 %looking at 25 random images
-	fname = [siftdir '/' fnames(fnames_vector_2(i)).name];
+	fname = [sift_dir '/' fnames(fnames_vector(i)).name];
 	load(fname, 'imname', 'descriptors', 'positions', 'scales', 'orients');
 	word_distance = dist2(words, descriptors);
 	[~,min_index1] = min(word_distance(1,:));
-	%[~,min_index2] = min(word_distance(2,:));
-	impath = [framesdir '/' imname];
+	impath = [frames_dir '/' imname];
 	im = imread(impath);
-	im = rgb2gray(im);
-	patch_1 = getPatchFromSIFTParameters(positions(min_index1,:),scales( min_index1), orients(min_index1), im);
-	%patch_2 = getPatchFromSIFTParameters(positions(min_index2,:),scales( min_index2), orients(min_index2), im);
+	patch = getPatchFromSIFTParameters(positions(min_index1,:),scales( min_index1), orients(min_index1), rgb2gray(im));
  
 	subplot(5,5,i);
-	imshow(patch_1);
+	imshow(patch);
 	
-% 	subplot(10,5,i+25);
-% 	imshow(patch_2);
 end
 
-figure(Word_2); 
-fnames_vector_3 = randperm(6612,25);
+figure(f2); 
+fnames_vector = randperm(num_feats,25);
 for i = 1:25 %looking at 25 random images
-	fname = [siftdir '/' fnames(fnames_vector_2(i)).name];
+	fname = [sift_dir '/' fnames(fnames_vector(i)).name];
 	load(fname, 'imname', 'descriptors', 'positions', 'scales', 'orients');
 	word_distance = dist2(words, descriptors);
-	%[~,min_index1] = min(word_distance(1,:));
 	[~,min_index2] = min(word_distance(2,:));
-	impath = [framesdir '/' imname];
+	impath = [frames_dir '/' imname];
 	im = imread(impath);
 	im = rgb2gray(im);
-	%patch_1 = getPatchFromSIFTParameters(positions(min_index1,:),scales( min_index1), orients(min_index1), im);
-	patch_2 = getPatchFromSIFTParameters(positions(min_index2,:),scales( min_index2), orients(min_index2), im);
+	patch = getPatchFromSIFTParameters(positions(min_index2,:),scales( min_index2), orients(min_index2), rgb2gray(im));
  
 	subplot(5,5,i);
-	imshow(patch_2);
+	imshow(patch);
 	
 end
 
